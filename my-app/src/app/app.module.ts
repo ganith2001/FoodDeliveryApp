1e25db277826da8874d98d24bc1cc23bccaad2fb
ganith2001 <ganith2001@gmail.com>
2022-07-02 16:04:05 +0530

    add changes

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,12 +5,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent  } from './app.component';
 
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { NavComponent } from './nav/nav.component';
 import {MatMenuModule} from '@angular/material/menu';
 
@@ -29,7 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material';
 
-import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { RouterModule} from '@angular/router';
 import { HomeComponent } from './home/home.component';
@@ -55,9 +55,9 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
+   
     MatToolbarModule,
-    FlexLayoutModule,
+    
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
@@ -67,7 +67,7 @@ import { ProfileComponent } from './profile/profile.component';
     MatGridListModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule,
+ 
     RouterModule,
     MatFormFieldModule,
     AppRoutingModule
