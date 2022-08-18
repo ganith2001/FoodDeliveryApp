cbc0de69f4d15bfaf8865ef8ae50326caf7d3408
ganith2001 <ganith2001@gmail.com>
2022-08-18 23:49:09 +0530

    FInal

--- my-app/src/app/cart/cart.component.ts
+++ my-app/src/app/cart/cart.component.ts
@@ -12,58 +12,35 @@ public quantity={};
 public cartitems:any;
 public total=0;
 
+constructor(private _menus:MenuService) { }
 
-
-  constructor(private _menus:MenuService) { }
-
-  ngOnInit() {
-  
-    
+ngOnInit() {
+      
    this._menus.findItems().subscribe(data=>{
     var i=0;
-
-    for(var val of data[0].menu){
-   
-  
+    for(var val of data[0].menu){  
       this.quantity[val.item]=val.quantity
-
- 
-      this._menus.findItemsDetails(val.item).subscribe(response=>{
-        
+      this._menus.findItemsDetails(val.item).subscribe(response=>{  
         this.cartitems=response;
         this.item.push(this.cartitems[0])
-        this.total=this.total+(this.cartitems[0].price*val.quantity);
-      
+        this.total=this.total+(this.cartitems[0].price*val.quantity);   
         i++;
-      })
-      
-      
-    }
-    
-      
-    
-    }) 
-  
-    
+      })    
+    }  
+  })  
+}
+
+
+onadd(id,quantity){   
+  if(quantity<5){
+      this._menus.update(id,quantity+1);  
   }
+}
 
-
-  onadd(id,quantity){
-    
-    if(quantity<5){
-
-      this._menus.update(id,quantity+1);
-
-   
-    }
+onsub(id,quantity){
+  if(quantity>1){
+        this._menus.update(id,quantity-1);   
   }
-
-  onsub(id,quantity){
-    if(quantity>1){
- 
-        this._menus.update(id,quantity-1);
-    
-    }
-  }
+}
 
 }
--- my-app/src/app/profile/profile.component.ts
+++ my-app/src/app/profile/profile.component.ts
@@ -13,56 +13,49 @@ phoneno:String;
 add:string;
 public error="";
 pwd={password1:'',password2:''};
-  constructor(private _menus:MenuService) { }
 
-  ngOnInit() {
-    this._menus.findUserDetails().subscribe(data=>{
-     
-  
-      this.user=data;
-  
-      
-      if(!this.user.phone){
-          this.phoneno="Phone-no";
-      }
-      else{
-        this.phoneno=this.user.phone;
-      }
-      if(!this.user.address){
-        this.add="address";
+constructor(private _menus:MenuService) { }
+
+ngOnInit() {
+  this._menus.findUserDetails().subscribe(data=>{ 
+    this.user=data;   
+    if(!this.user.phone){
+      this.phoneno="Phone-no";
+    }
+    else{
+      this.phoneno=this.user.phone;
+    }
+    if(!this.user.address){
+      this.add="address";
     }
     else{
       this.add=this.user.address;
-    }
-     
-    })
-  }
-  phone(){
-    this._menus.updatePhone(this.userphone.phno)
-    
-  }
+    } 
+  })
+}
 
-  address(){
-    this._menus.updateAdress(this.useradd.add)
-    
-  }
+phone(){
+  this._menus.updatePhone(this.userphone.phno)    
+}
+
+address(){
+    this._menus.updateAdress(this.useradd.add)    
+}
+
 password(){
-
   const regex = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$');
   const valid = regex.test(this.pwd.password1);
-
   if(valid){
-if(this.pwd.password1==this.pwd.password2){
-  this._menus.updatepassword(this.pwd.password1)
+    if(this.pwd.password1==this.pwd.password2){
+      this._menus.updatepassword(this.pwd.password1)
+    }
+  else{
+    this.error="Password and confirmed password doesnt match";
+  }
+  }
+  else{
+    this.error="Password should have minimum 8 characters,at least 1 uppercase,1 lowercase letter and 1 number";
+  }
 }
-else{
-  this.error="Password and confirmed password doesnt match";
-}
-}
-else{
-  this.error="Password should have minimum 8 characters,at least 1 uppercase,1 lowercase letter and 1 number";
-}
-}
-
 
 }
