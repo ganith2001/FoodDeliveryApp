0499825b1c63daade364f72bd5c31c0ca07a29a3
ganith2001 <ganith2001@gmail.com>
2022-08-18 19:11:53 +0530

    app.js update

--- my-app/backend/app.js
+++ my-app/backend/app.js
@@ -17,6 +17,7 @@ mongoose.connect('mongodb+srv://Ganith:ganith2001@cluster0.wz8tp.mongodb.net/myM
 
 const bodyParser = require('body-parser');
 const { RSA_NO_PADDING } = require('constants');
+const { ConsoleReporter } = require('jasmine');
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
@@ -36,35 +37,24 @@ app.use((req,res,next)=>{
 
 
 app.get("/api/posts",(req,res,next)=>{
-
-    menus.find( ).then(documents => {
-       console.log(documents)
+    menus.find( ).then(documents => { 
         res.send(documents);
-      
-      
     })
-     
-  }); 
+}); 
 
-  app.get("/api/posts/nonveg",(req,res,next)=>{
-   
-     menus.find( { nonveg: "yes" } ).then(documents => {
-         res.send(documents);    
-     })
-      
-   }); 
+app.get("/api/posts/nonveg",(req,res,next)=>{   
+    menus.find( { nonveg: "yes" } ).then(documents => {
+        res.send(documents);    
+    })
+}); 
 
-   app.get("/api/posts/veg",(req,res,next)=>{  
-     menus.find( { nonveg: "no" } ).then(documents => {
-       
-         res.send(documents);     
-     })
-      
-   }); 
+app.get("/api/posts/veg",(req,res,next)=>{  
+    menus.find( { nonveg: "no" } ).then(documents => {
+       res.send(documents);     
+    })
+}); 
  
-   app.post("/signup",(req,res,next)=>{
- 
-
+app.post("/signup",(req,res,next)=>{
     bcrypt.hash(req.body.password,10)
     .then(hash =>{ 
         const user = new User({ 
@@ -92,12 +82,9 @@ app.get("/api/posts",(req,res,next)=>{
 app.post('/login',(req,res,next) => {
     let fetcheduser;
     User.findOne({email:req.body.email}).then(user =>
-        {
-        
-          
-            fetcheduser=user;
+    {
+        fetcheduser=user;
         return bcrypt.compare(req.body.password,user.password);
-
         })
         .then(result => {
        
@@ -121,115 +108,88 @@ app.post('/login',(req,res,next) => {
             message:"Auth failed"
         });
         });
-    }
-    );
+    });
 
-    app.post('/token',(req,res,next)=>{
-        
-        
-        const user=jwt.verify(req.body.token,"secret_this_should_be_longer");
-                               
-        
-          
-    
-        User.findOne({email:user.email}).then(user =>
-            {
-                
+app.post('/token',(req,res,next)=>{        
+    const user=jwt.verify(req.body.token,"secret_this_should_be_longer");
+    User.findOne({email:user.email}).then(user =>
+    {
+        res.send(user);
+    })
+})
 
-               res.send(user);
-           
-            })
-            
-        })
+app.post('/addcart',(req,res,next)=>{
+    const filter={_id:req.body.name}
+    const update={item:req.body.item,quantity:1}
+    User.findOneAndUpdate(filter,{$push:{menu:update}}).then(result=>
+    {
+        res.send(result);
+    })
+})
 
-    app.post('/addcart',(req,res,next)=>{
-
-        const filter={_id:req.body.name}
-        const update={item:req.body.item,quantity:1}
-          
-        User.findOneAndUpdate(filter,{$push:{menu:update}}).then(result=>{
+app.post('/findItem',(req,res,next)=>{
+    const user=jwt.verify(req.body.token,"secret_this_should_be_longer"); 
+    User.find({email:user.email},{menu:1,_id:0}).then(result=>
+    {
             res.send(result);
-        })
     })
+})
 
-    app.post('/findItem',(req,res,next)=>{
-   
-    
-        User.find({_id:req.body.id},{menu:1,_id:0}).then(result=>{
+app.post('/findDetails',(req,res,next)=>{    
+    menus.find({_id:req.body.id}).then(result=>
+    {
+        res.send(result);
+    })
+})
 
+app.post('/update',(req,res,next)=>{      
+    User.updateOne({_id:req.body.userid,'menu.item':req.body.menuid},{$set:{
+       'menu.$.quantity':req.body.quan}})
+    .then(result=>{
             res.send(result);
-        })
-    })
+     })
+})
 
-    app.post('/findDetails',(req,res,next)=>{
-    
-        menus.find({_id:req.body.id}).then(result=>{
-   
-            res.send(result);
-        })
+app.post('/removeItem',(req,res,next)=>{       
+    User.updateOne({_id:req.body.userid },{ $pull: { menu: { item:req.body.menuid } } })
+    .then(result=>{       
+        res.send(result);
     })
-
-    app.post('/update',(req,res,next)=>{
-      
-        User.updateOne({_id:req.body.userid,'menu.item':req.body.menuid},{$set:{
-            'menu.$.quantity':req.body.quan
-        }}).then(result=>{
-           
-        })
-    })
-
-    app.post('/removeItem',(req,res,next)=>{
-       
-        User.updateOne(
-            {_id:req.body.userid },
-            { $pull: { menu: { item:req.body.menuid } } }
-          ).then(result=>{
-           
-        
-          })
-    })
+})
  
-    app.post('/profile',(req,res,next)=>{
-      
-        User.find({_id:req.body.id}).then(result=>{
-      
-            res.send(result);
-        })
+app.post('/profile',(req,res,next)=>{
+    const user=jwt.verify(req.body.token,"secret_this_should_be_longer"); 
+    User.findOne({email:user.email})
+    .then(user =>   
+    {
+        res.send(user);
     })
-    app.post('/phone',(req,res,next)=>{
-      
-        User.updateOne({_id:req.body.id },{phone:req.body.phone}).then(result=>{
-            
-            res.send(result);
-        
-        })
-    })
+})
 
-    app.post('/ad',(req,res,next)=>{
-      
-        User.updateOne({_id:req.body.id },{address:req.body.add}).then(result=>{
     
-            res.send(result);
-        
-        })
+app.post('/phone',(req,res,next)=>{      
+    User.updateOne({_id:req.body.id },{phone:req.body.phone}).then(result=>{        
+        res.send(result);    
     })
+})
 
-    app.post('/passw',(req,res,next)=>{
+app.post('/ad',(req,res,next)=>{      
+    User.updateOne({_id:req.body.id },{address:req.body.add}).then(result=>{
+        res.send(result);   
+    })
+})
 
-   
-        bcrypt.hash(req.body.password,10)
+app.post('/passw',(req,res,next)=>{
+    bcrypt.hash(req.body.password,10)
     .then(hash =>{ 
-        User.updateOne({_id:req.body.id },{password:hash}).then(result=>{
-        
+        User.updateOne({_id:req.body.id },{password:hash})
+        .then(result=>{    
             res.send(result);
-        
         })
     })
-    })
+})
 
 
-  app.listen(8080);
+app.listen(3000);
 
-
-  
-  module.exports = app;
+module.exports = app;
